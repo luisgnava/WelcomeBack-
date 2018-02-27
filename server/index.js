966b239666ec3276748607da3675cf9c6e4226c9
abimaelhernandez <hernandezabimael364@gmail.com>
2018-02-27 13:56:01 -0600

    test

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
 
-app.use(bodyParser.json());
- app.use(express.static(__dirname + '/../client/dist'));
 
-//get request
+app.get('/', function(req, res){
+  res.send(console.log('hey hola'))
+});
 
-app.listen(3000, ()=>{
-  console.log('listening on 3000')
-})
+
+
+
+app.listen(3000, function() {
+  console.log('Server started and listening on port 3000');
+});
