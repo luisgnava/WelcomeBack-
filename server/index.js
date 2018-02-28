bb8420a29421de8fa79b5eb47ddbbb9e28ef56ca
abimaelhernandez <hernandezabimael364@gmail.com>
2018-02-27 20:00:46 -0600

    app is now rendering

--- server/index.js
+++ server/index.js
@@ -1,13 +1,7 @@
 var express = require('express');
 var app = express();
 
-
-app.get('/', function(req, res){
-  res.send(console.log('hey hola'))
-});
-
-
-
+app.use(express.static(__dirname + '/../client/dist'));
 
 app.listen(3000, function() {
   console.log('Server started and listening on port 3000');
