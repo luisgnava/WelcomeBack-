63b7a23426d81eaa0cdd5b4cd38f96eb604b6a1b
luisgnava <luis.gnava85@gmail.com>
2018-02-28 15:45:19 -0600

    merged files conflict is no longer a conflict

--- server/index.js
+++ server/index.js
@@ -3,10 +3,15 @@ var bodyParser = require('body-parser');
 var app = express();
 
 app.use(bodyParser.json());
- app.use(express.static(__dirname + '/../client/dist'));
+ app.use(express.static(__dirname + '/../src/components'));
 
 //get request
+app.get(bodyParser.json());
+app.get('/', function(req, res){
+  res.send();
+});
 
-app.listen(3000, ()=>{
+
+app.listen(3000, () => {
   console.log('listening on 3000')
 })
