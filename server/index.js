b6f09b9c7ef6fb341f7012e879e8ede9ed55998b
Eduardo091 <urrutia.e14@gmail.com>
2018-03-01 17:31:59 -0600

    added proper gitignore

--- server/index.js
+++ server/index.js
@@ -5,7 +5,34 @@ var app = express();
 app.use(bodyParser.json());
  app.use(express.static(__dirname + '/../client/dist'));
 
-//get request
+app.get('/paisa', function(req,res){
+  data.getComment((err,results)=>{
+if(err){
+  console.log('error')
+  res.sendStatus(500);
+}else{
+  res.status(200).json(results)
+}
+})
+} )
+
+app.post('/paisa', function(req,res){
+  let comment =req.body.comment
+  if(!comment){
+    res.sendStatus(400);
+
+  }else{
+    data.postComment(comment,(err,results)=>{
+      if(err){
+        res.sendStatus(500);
+
+      }else{
+        res.status(200).json(results);
+      }
+    })
+  }
+})
+
 
 app.listen(3000, ()=>{
   console.log('listening on 3000')
